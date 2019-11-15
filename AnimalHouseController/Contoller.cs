5126a186224057e8bea892e45efbe1f0bac74a99
Viggo Lysdahl <Viggo_lysdahl@hotmail.com>
2019-11-15 11:06:14 +0100

    Created all emty layer

---
+++ AnimalHouseController/Contoller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHouseController
+{
+    class Contoller
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ AnimalHouseV3/Form Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalHouseV3
+{
+    public partial class Form_Animal : Form
+    {
+        public Form_Animal()
+        {
+            InitializeComponent();
+        }
+    }
+}
