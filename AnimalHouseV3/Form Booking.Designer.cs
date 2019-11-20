23ccb0e00bef03eb5214772a4c988a490377ee5e
Viggo Lysdahl <Viggo_lysdahl@hotmail.com>
2019-11-20 10:05:05 +0100

    Lavet en Main til temp filer

--- AnimalHouseV3/Form Booking.Designer.cs
+++ AnimalHouseV3/Form Booking.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form_Booking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(942, 526);
+            this.Name = "Form_Booking";
             this.Text = "Form_Booking";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
