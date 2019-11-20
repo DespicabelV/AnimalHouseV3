1f0bea41e24f78f3e9148c814179b10dbc846f87
Jacob Ladegaard Friis Lageri <jaco7925@edu.eal.dk>
2019-11-20 09:12:10 +0100

    Lavet UI til owner

--- AnimalHouseV3/Form Owner.cs
+++ AnimalHouseV3/Form Owner.cs
@@ -16,5 +16,10 @@ namespace AnimalHouseV3
         {
             InitializeComponent();
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
