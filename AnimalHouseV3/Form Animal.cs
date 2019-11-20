e5c45ad1c591c21f33367d15fe0511be088f9a5d
Allan Thanabalasingam <alla2265@edu.eal.dk>
2019-11-20 09:45:41 +0100

    Dyr UI er færdiggjort. metoden til indsættelse af dyr i databasen er under konstruktion.

--- AnimalHouseV3/Form Animal.cs
+++ AnimalHouseV3/Form Animal.cs
@@ -16,5 +16,32 @@ namespace AnimalHouseV3
         {
             InitializeComponent();
         }
+
+        private void Form_Animal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtDyrBirthday_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnimalCreate_Click(object sender, EventArgs e)
+        {
+            if (TxtAnimalName.Text==""||TxtAnimalRace.Text==""||AnimalBirthCalender.Value==null|| TxtBoxAnimalNr.Text==""||TxtOwnerName.Text==""||TxtBoxAnimalDoctorName.Text==""||TxtBoxAnimalChip.Text=="")
+            {
+                MessageBox.Show("Fill out the blanks");
+            }
+            else
+            {
+                Insert(TxtAnimalName.Text, TxtAnimalRace.Text, AnimalBirthCalender.Value, Convert.ToInt32(TxtBoxAnimalNr.Text), TxtOwnerName.Text, TxtBoxAnimalDoctorName.Text, Convert.ToInt32(TxtBoxAnimalChip.Text));
+            }
+        }
     }
 }
